ad8ffe5c26eb0cbbbf1a9f6140d41f815e162e6a
Alaa Obeid <alaaobed88@gmail.com>
2023-04-21 02:18:24 +0300

    finished main banner component with styling, also deleted card comp

--- 01-starting-project/src/components/MainBanner/MainBanner.js
+++ 01-starting-project/src/components/MainBanner/MainBanner.js
@@ -1,13 +1,18 @@
 import styles from "./MainBanner.module.css";
-import Card from "../UI/Card";
+
 const MainBanner = () => {
   return (
-    <Card>
-      <div className={styles["main-banner"]}>
-        <h2>Delicious Food, Delivered To You</h2>
-        <p>Choose your favorite meal from our abroad selection of available meals and enjoy a delicious lunch or dinner at home.<br/>all our meals are cooked with high quality ingredients, just-in-time and of course by professianol chefs</p>
-      </div>
-    </Card>
+    <div className={styles["main-banner"]}>
+      <h2>Delicious Food, Delivered To You</h2>
+      <p>
+        Choose your favorite meal from our abroad selection of available meals
+        and enjoy a delicious lunch or dinner at home.
+      </p>
+      <p>
+        All our meals are cooked with high quality ingredients, just-in-time and
+        of course by professional chefs
+      </p>
+    </div>
   );
 };
 
